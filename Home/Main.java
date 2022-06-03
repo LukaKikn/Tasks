bfade21553bd632badeaeab4336465b8d50c5444
LukaKikn <91747518+LukaKikn@users.noreply.github.com>
2022-06-03 12:09:32 +0400

    Add files via upload

--- Home/Main.java
+++ Home/Main.java
@@ -13,11 +13,16 @@ public class Main {
         System.out.println(f1 instanceof Rectangle);
         System.out.println(r1 instanceof Rectangle);
         System.out.println(f1 instanceof Figure);
+        System.out.println(s1 instanceof Figure);
+        System.out.println(s1 instanceof Rectangle);
+
 
         System.out.println(r1.getPerimeter());
         System.out.println(r1.getArea());
-        System.out.println(r1.getPerimeter(7,5));
+        System.out.println(r1.getPerimeter(12.5,2.5));
         System.out.println(r1.getArea(12.5,2.5));
+        System.out.println(s1.getPerimeter(6,4));
+        System.out.println(s1.getArea(6,4));
         Figure.sayHello();
 
 
